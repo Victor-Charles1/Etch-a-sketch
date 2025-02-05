6e80175d7a6e7778918dee6bedd58cf4ee248593
Victor-Charles1 <91386255+Victor-Charles1@users.noreply.github.com>
2025-02-05 12:25:31 -0500

    fixed error in css: referenced class instead of id

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 
 const container = document.querySelector('#container')
 
-let newDiv= document.createElement('div')
-container.appendChild(newDiv)
 
-for(i=1; i<16;i++){
+
+for(i=0; i<256;i++){
     let newDiv= document.createElement('div')
     newDiv.classList.add('square')
     container.appendChild(newDiv)
