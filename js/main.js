be16ba2921f7e74c8181d5c80ab04b1fabba0471
Victor-Charles1 <91386255+Victor-Charles1@users.noreply.github.com>
2025-02-05 15:42:04 -0500

    added eventListner for color change on hover effect

--- js/main.js
+++ js/main.js
@@ -7,5 +7,19 @@ for(i=0; i<256;i++){
     let newDiv= document.createElement('div')
     newDiv.classList.add('square')
     container.appendChild(newDiv)
-console.log('new div created')
+    console.log('new div created')
+    
 }
+
+// event listener to change color of divs with class = square
+const write =document.querySelector('.square')
+
+write.addEventListener('mouseover',function(){
+    write.style.backgroundColor ='yellow';
+    console.log('I changed the background color with javascript')
+});
+write.addEventListener('mouseout',function(){
+    write.style.backgroundColor ='yellow';
+    console.log('I changed the background color with javascript')
+});
+
