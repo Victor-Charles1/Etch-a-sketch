e2d3de82e1c5e3e32aa065d59dd29b69a96c2e1f
Victor-Charles1 <91386255+Victor-Charles1@users.noreply.github.com>
2025-02-09 15:00:19 -0500

    added progressively darkening effect and input validation

--- js/main.js
+++ js/main.js
@@ -1,33 +1,74 @@
 
 const container = document.querySelector('#container')
+const btn = document.querySelector('#btn')
 
-// prompt('Enter number of squares per side for the new grid.')
-
-for(i=0; i<256;i++){
-    let newDiv= document.createElement('div')
-    newDiv.classList.add('square')
-    container.appendChild(newDiv)
-    console.log('new div created')
-    
-}
 
 // event listener to change color of divs with class = square
-const write =document.querySelectorAll('.square')
 
-write.forEach(function(square) {
-    square.addEventListener('mouseover', function() {
-        square.style.backgroundColor = 'yellow';
-        console.log('I changed the background color with JavaScript');
+
+function changeColor(){
+    const write =document.querySelectorAll('.square')
+    write.forEach(function(square) {
+        square.dataset.opacity = 0;
+        square.addEventListener('mouseover', function() {
+            square.style.backgroundColor = 'yellow';
+
+            
+           
+            console.log('I changed the background color with JavaScript');
+        });
+    
+        square.addEventListener('mouseout', function() {
+            square.style.backgroundColor = 'black';
+            console.log('I changed the background color with JavaScript');
+         
+            let currentOpacity = parseFloat(square.dataset.opacity);
+            if (currentOpacity < 1) {
+                currentOpacity += 0.1;
+                square.dataset.opacity = currentOpacity;
+
+                square.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity})`;
+            }
+        });
     });
+    
+    }
 
-    square.addEventListener('mouseout', function() {
-        square.style.backgroundColor = 'yellow';
-        console.log('I changed the background color with JavaScript');
-    });
-});
 
-const btn = document.querySelector('#btn')
+//btn prompt and input validation
 btn.addEventListener('click', function(){
-   let choice=  prompt('Enter number of squares per side for the new grid.')
-   console.log(choice)
+   let response=  prompt('Enter number of squares per side for the new grid.')
+   console.log(response)
+   response=parseInt(response)
+
+   if(Number.isNaN(response)||response>100|| response<0){
+    alert('Enter a number less than 100.')
+   }else{
+    createGrid(response)
+   }
 });
+
+//create grid function
+function createGrid(squaresPerSide){
+    container.innerHTML=''; // remove old grid
+
+    const containerSize = container.clientWidth
+    const squareSize = containerSize/squaresPerSide
+
+
+
+
+//for loop makes grid
+    for(i=0; i<squaresPerSide*squaresPerSide;i++){
+        let newDiv= document.createElement('div')
+        newDiv.classList.add('square')
+        newDiv.style.width=`${squareSize}px`
+        newDiv.style.height=`${squareSize}px`
+        container.appendChild(newDiv)
+        console.log('new div created')
+        
+        
+    }
+    changeColor()
+}
+createGrid(16); //default 16x16 grid
